30b69397d8e67510d198af2bbf0e81b4ac55165e
cobject <cobject@gmail.com>
2019-04-15 14:34:33 +0900

    feature: Add Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, TouchableOpacity, Text, TextInput, Platform } from 'react-native'
+import { styles } from '../utils/styles'
 
 class Deck extends Component {
+
+    handleAddCard = () => {
+
+    }
+
+    handleStartQuiz = () => {
+
+    }
+
     render() {
+        const title = 'new deck'
+        const count = 10
+
         return (
-            <View>
-                <Text>
-                    Deck
-                </Text>
-            </View>
+        <View>
+            <Text>{title}</Text>
+            <Text>{count} cards</Text>
+            <TouchableOpacity
+                style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+                onPress={this.handleAddCard}>
+                <Text style={styles.submitBtnText}>Add Card</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+                onPress={this.handleStartQuiz}>
+                <Text style={styles.submitBtnText}>Start Quiz</Text>
+            </TouchableOpacity>
+        </View>
         )
     }
 }
