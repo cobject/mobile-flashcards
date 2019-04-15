a575bca7f3552b1e4c38bff4effed0225e352c86
cobject <cobject@gmail.com>
2019-04-15 14:50:06 +0900

    feature: Add basic DeckList component

--- components/DeckList.js
+++ components/DeckList.js
@@ -2,12 +2,46 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class DeckList extends Component {
+    state = {
+        decks: {
+            aaa: {
+                id: 'aaa',
+                title: 'new deck',
+                cards: [
+                    {
+                        question: 'qqq',
+                        answer: 'aaa'
+                    }
+                ]
+            },
+            bbb: {
+                id: 'bbb',
+                title: 'new deck2',
+                cards: [
+                    {
+                        question: 'qqq',
+                        answer: 'aaa'
+                    }
+                ]
+            }
+        }
+    }
+
     render() {
         return (
             <View>
-                <Text>
-                    DeckList
-                </Text>
+                {Object.keys(this.state.decks).map((key) => {
+                    return (
+                        <View>
+                            <Text>
+                                {this.state.decks[key].title}
+                            </Text>
+                            <Text>
+                                {this.state.decks[key].cards.length}
+                            </Text>
+                        </View>
+                    )
+                })}
             </View>
         )
     }
